aff74ffaf21a748d7f20c7deb4fa10c2501d67e7
kid <963262214@qq.com>
2012-06-16 13:53:31 +0800

    增加了第二关和第三关

---
+++ CGlassTD/CGlassTD/StagePass2.cpp
@@ -0,0 +1,34 @@
+#include "StagePass2.h"
+#include "StagePass1Step1.h"
+#include "StagePass1Step0.h"
+#include <OgreLogManager.h>
+#include "UFO.h"
+#include "ParamParser.h"
+
+StagePass2::StagePass2(Ogre::SceneManager* sceneManager, StageManager* stageManager, MyGUI::Gui* gui)
+	: LevelStage(sceneManager, stageManager, gui)
+{
+	/// 初始化大炮
+	initializeCannon("CannonDefine.xml", "BulletDefine.xml");
+	/// 初始化迷宫
+	initializeMaze("MazeDefine.xml", "CellDefine.xml");
+	/// 初始化飞船
+	initializeUFO("UFODefine.xml");
+
+	/// 新增一个monster管理器
+	MonsterManager::initialize(mMaze, "MonsterDefine.xml");
+	mMonsterManager = MonsterManager::getMonsterManager();
+	/// 初始化一波一波怪物的参数,每层的波数不一样
+	mMonsterManager->setMonsterWave("monsterWave.xml");
+
+	/// 设置天空盒
+	//mSceneManager->setSkyBox(true, "Examples/EveSpaceSkyBox");
+	
+	mCurrentStep = new StagePass1Step0(this);
+	mCurrentStep->init();
+}
+
+
+StagePass2::~StagePass2(void)
+{
+}
---
+++ CGlassTD/CGlassTD/StagePass2.h
@@ -0,0 +1,20 @@
+#ifndef StagePass2_h__
+#define StagePass2_h__
+
+#include "LevelStage.h"
+
+/// 第一关场景
+class StagePass2 : public LevelStage
+{
+private:
+
+public:
+	/// 构造函数
+	/// @param pSceneManager 场景管理器的指针
+	/// @param pStageManager 舞台管理器的指针
+	StagePass2(Ogre::SceneManager* sceneManager, StageManager* stageManager, MyGUI::Gui* gui);
+	~StagePass2(void);
+};
+
+#endif // StagePass1_h__
+
