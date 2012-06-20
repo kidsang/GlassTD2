3597966da1139a47a3ecd19d58a3b545f21604f4
Liao Nanhao <670433522@qq.com>
2012-06-20 15:10:19 +0800

    添加按钮特效

--- CGlassTD/CGlassTD/StartStage.cpp
+++ CGlassTD/CGlassTD/StartStage.cpp
@@ -5,29 +5,37 @@
 StartStage::StartStage(Ogre::SceneManager* sceneManager, StageManager* stageManager, MyGUI::Gui* gui)
 	: Stage(sceneManager, stageManager, gui)
 {
+	flag = 0;
+	preFlag = -1;
 	mLayout = MyGUI::LayoutManager::getInstance().loadLayout("start.layout");
+	background = this->getGUI()->findWidget<MyGUI::ImageBox>("background"); 
+	background->eventMouseMove += MyGUI::newDelegate(this, &StartStage::notifyMouseMove);
 	juqingBtn = this->getGUI()->findWidget<MyGUI::ImageBox>("juqingMode"); 
 	lifeBtn = this->getGUI()->findWidget<MyGUI::ImageBox>("lifeMode"); 
 	settingBtn = this->getGUI()->findWidget<MyGUI::ImageBox>("setting"); 
 	quitBtn = this->getGUI()->findWidget<MyGUI::ImageBox>("quit"); 
-
+	juqingBtn->eventMouseMove += MyGUI::newDelegate(this, &StartStage::notifyMouseMove);
 	juqingBtn->eventMouseButtonPressed += MyGUI::newDelegate(this, &StartStage::notifyMouseButtonPress);
 	juqingBtn->eventMouseButtonReleased += MyGUI::newDelegate(this, &StartStage::notifyMouseButtonRelease);
-
+	lifeBtn->eventMouseMove += MyGUI::newDelegate(this, &StartStage::notifyMouseMove);
 	lifeBtn->eventMouseButtonPressed += MyGUI::newDelegate(this, &StartStage::notifyMouseButtonPress);
 	lifeBtn->eventMouseButtonReleased += MyGUI::newDelegate(this, &StartStage::notifyMouseButtonRelease);
-
+	settingBtn->eventMouseMove += MyGUI::newDelegate(this, &StartStage::notifyMouseMove);
 	settingBtn->eventMouseButtonPressed += MyGUI::newDelegate(this, &StartStage::notifyMouseButtonPress);
 	settingBtn->eventMouseButtonReleased += MyGUI::newDelegate(this, &StartStage::notifyMouseButtonRelease);
-	
+	quitBtn->eventMouseMove += MyGUI::newDelegate(this, &StartStage::notifyMouseMove);
 	quitBtn->eventMouseButtonPressed += MyGUI::newDelegate(this, &StartStage::notifyMouseButtonPress);
 	quitBtn->eventMouseButtonReleased += MyGUI::newDelegate(this, &StartStage::notifyMouseButtonRelease);
 }
 
+void StartStage::notifyMouseMove( MyGUI::Widget* _sender, int _left, int _top )
+{
+	
+}
 void StartStage::notifyMouseButtonPress( MyGUI::Widget* _sender, int _left, int _top , MyGUI::MouseButton _id)
 {
 	if(_sender == juqingBtn)
-	{
+	{		
 		juqingBtn->setImageTexture("juqingPress.png");
 	}
 	else if(_sender == lifeBtn)
@@ -94,6 +102,8 @@ bool StartStage::onMouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID 
 {
 	return true;
 }
+
+
 
 
 
