610c601556a44d094b0296b2f129e664922315d8
Liao Nanhao <670433522@qq.com>
2012-06-28 11:32:11 +0800

    添加素材

--- CGlassTD/CGlassTD/GameResource.cpp
+++ CGlassTD/CGlassTD/GameResource.cpp
@@ -4,14 +4,14 @@ Ogre::String GameResource::WORD_OF_STAGE[6] = {
 	"word_stage1.png",
 	"word_stage2.png",
 	"word_stage3.png",
-	"word_stage1.png",
-	"word_stage2.png",
-	"word_stage3.png"
+	"word_stage4.png",
+	"word_stage5.png",
+	"word_stage6.png"
 };
 
 Ogre::String GameResource::WORD_RESULT_SUCCESS = "sucess.png";
 Ogre::String GameResource::WORD_RESULT_FAIL = "fail.png";
-Ogre::String GameResource::WORD_RESULT_PAUSE = "fail.png";
+Ogre::String GameResource::WORD_RESULT_PAUSE = "pause.png";
 
 GameResource::GameResource(void)
 {
